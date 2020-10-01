162f695b3357f9ad602324617c7d34da76f5230e
JinleeJeong <pjjr0118@gmail.com>
2020-10-01 16:12:54 +0900

    connect firestore for nwitter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { dbService } from "fbase";
 
-const Home = () => <span>Home</span>;
-export default Home;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+        const nweetObject = {
+            ...document.data(),
+            id: document.id,
+        };
+        setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
+    
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      await dbService.collection("nweets").add({
+        nweet,
+        createdAt: Date.now(),
+      });
+      setNweet("");
+    };
+    const onChange = (event) => {
+      const {
+        target: { value },
+      } = event;
+      setNweet(value);
+    };
+    return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            value={nweet}
+            onChange={onChange}
+            type="text"
+            placeholder="What's on your mind?"
+            maxLength={120}
+          />
+          <input type="submit" value="Nweet" />
+        </form>
+
+        <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+        </div>
+
+      </div>
+    );
+  };
+  export default Home;
