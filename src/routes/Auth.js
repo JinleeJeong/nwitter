7b4ee9db3b8be2ac919f95868d139fc09b6924fe
JinleeJeong <pjjr0118@gmail.com>
2020-10-01 14:29:09 +0900

    2.5 commit for social Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -38,6 +38,20 @@ const Auth = () => {
 
   const toggleAccount = () => setNewAccount((prev) => !prev);
   
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
   return (
     <div>
         <form onSubmit={onSubmit}>
@@ -69,8 +83,12 @@ const Auth = () => {
         </span>
     
         <div>
-            <button>Continue with Google</button>
-            <button>Continue with Github</button>
+            <button onClick={onSocialClick} name="google">
+            Continue with Google
+            </button>
+            <button onClick={onSocialClick} name="github">
+            Continue with Github
+            </button>
         </div>
       </div>
     );
