fcb77acffeb4ed38e2cffe8fe6d78221d55ab84e
JinleeJeong <pjjr0118@gmail.com>
2020-10-01 16:41:23 +0900

    #3.6 Nwitting add CRUD and check uid

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
+        setUserObj(user);
       } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
@@ -18,7 +18,11 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? (
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,28 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
+import Nweet from "components/Nweet";
 
-const Home = () => {
+const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    const getNweets = async () => {
-        const dbNweets = await dbService.collection("nweets").get();
-        dbNweets.forEach((document) => {
-        const nweetObject = {
-            ...document.data(),
-            id: document.id,
-        };
-        setNweets((prev) => [nweetObject, ...prev]);
-        });
-    };
+
     useEffect(() => {
-        getNweets();
+        dbService.collection("nweets").onSnapshot((snapshot) => {
+            const nweetArray = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }));
+            setNweets(nweetArray);
+          });
     }, []);
     
     const onSubmit = async (event) => {
       event.preventDefault();
       await dbService.collection("nweets").add({
-        nweet,
+        text : nweet,
         createdAt: Date.now(),
+        creatorId: userObj.uid,
       });
       setNweet("");
     };
@@ -47,9 +46,11 @@ const Home = () => {
 
         <div>
         {nweets.map((nweet) => (
-          <div key={nweet.id}>
-            <h4>{nweet.nweet}</h4>
-          </div>
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+            />
         ))}
         </div>
 
