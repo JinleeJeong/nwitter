2fd9a4d48a3c8d90214c72e944676a83df207a8c
JinleeJeong <pjjr0118@gmail.com>
2020-10-01 13:55:21 +0900

    #2.4 commit for login

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
 
-  console.log(isLoggedIn);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -31,33 +32,45 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  
   return (
     <div>
         <form onSubmit={onSubmit}>
-          <input
-            name="email"
-            type="email"
-            placeholder="Email"
-            required
-            value={email}
-            onChange={onChange}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Password"
-            required
-            value={password}
-            onChange={onChange}
-          />
-          <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+            <input
+                name="email"
+                type="email"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={onChange}
+            />
+            <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={onChange}
+            />
+            <input
+            type="submit"
+            value={newAccount ? "Create Account" : "Sign In"}
+            />
+            {error}
         </form>
+
+        <span onClick={toggleAccount}>
+            {newAccount ? "Sign In" : "Create Account"}
+        </span>
+    
         <div>
-          <button>Continue with Google</button>
-          <button>Continue with Github</button>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
         </div>
       </div>
     );
