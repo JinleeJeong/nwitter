868039d363dee330ed5483c2b961513ac4684665
JinleeJeong <pjjr0118@gmail.com>
2020-10-02 18:10:50 +0900

    #5 Edit Profile and realtime Rendering

--- src/components/Router.js
+++ src/components/Router.js
@@ -8,7 +8,7 @@ import Navigation from "components/Navigation";
 const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation />}
+      {isLoggedIn && <Navigation userObj={userObj}/>}
       <Switch>
         {isLoggedIn ? (
           <>
@@ -16,7 +16,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
               <Home userObj={userObj}/>
             </Route>
             <Route exact path="/profile">
-              <Profile />
+              <Profile userObj={userObj} />
             </Route>
 
           </>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { authService } from "fbase";
 import { useHistory } from "react-router-dom";
 
-export default () => {
-  const history = useHistory();
-  const onLogOutClick = () => {
-    authService.signOut();
-    history.push("/");
-  };
+export default ({ userObj }) => {
+
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
+    const history = useHistory();
+    const onLogOutClick = () => {
+      authService.signOut();
+      history.push("/");
+    };
+    
+    /*const getMyNweets = async () => {
+      const nweets = await dbService
+        .collection("nweets")
+        .where("creatorId", "==", userObj.uid)
+        .orderBy("createdAt")
+        .get();
+      console.log(nweets.docs.map((doc) => doc.data()));
+    };*/
+  
+    const onChange = (event) => {
+        const {
+          target: { value },
+        } = event;
+        setNewDisplayName(value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+            displayName: newDisplayName,
+            });
+        }
+    }
+
   return (
     <>
-      <button onClick={onLogOutClick}>Log Out</button>
+        <form onSubmit={onSubmit}>
+            <input
+            onChange={onChange}
+            type="text"
+            placeholder="Display name"
+            value={newDisplayName}
+            />
+            <input type="submit" value="Update Profile" />
+        </form>
+        <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
 };
