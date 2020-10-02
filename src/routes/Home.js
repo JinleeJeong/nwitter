36d4ea95e393ebceaf4a74d032f42d49b9802cee
JinleeJeong <pjjr0118@gmail.com>
2020-10-02 16:41:34 +0900

    #4.3 Uplode of the File & read for Preview

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { dbService } from "fbase";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
 import Nweet from "components/Nweet";
 
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     useEffect(() => {
         dbService.collection("nweets").onSnapshot((snapshot) => {
@@ -18,12 +20,24 @@ const Home = ({userObj}) => {
     
     const onSubmit = async (event) => {
       event.preventDefault();
-      await dbService.collection("nweets").add({
-        text : nweet,
+
+      let attachmentUrl = "";
+      if (attachment !== "") {
+        const attachmentRef = storageService
+          .ref()
+          .child(`${userObj.uid}/${uuidv4()}`);
+        const response = await attachmentRef.putString(attachment, "data_url");
+        attachmentUrl = await response.ref.getDownloadURL();
+      }
+      const nweetObj = {
+        text: nweet,
         createdAt: Date.now(),
         creatorId: userObj.uid,
-      });
+        attachmentUrl,
+      };
+      await dbService.collection("nweets").add(nweetObj);
       setNweet("");
+      setAttachment("");
     };
     const onChange = (event) => {
       const {
@@ -39,23 +53,36 @@ const Home = ({userObj}) => {
         const theFile = files[0];
         const reader = new FileReader();
         reader.onloadend = (finishedEvent) => {
-          console.log(finishedEvent);
+            const {
+                currentTarget: { result },
+            } = finishedEvent;
+            setAttachment(result);
+            
         };
         reader.readAsDataURL(theFile);
     };
 
+    const onClearAttachment = () => setAttachment(null);
+
     return (
       <div>
         <form onSubmit={onSubmit}>
-          <input
-            value={nweet}
-            onChange={onChange}
-            type="text"
-            placeholder="What's on your mind?"
-            maxLength={120}
-          />
-          <input type="submit" value="Nweet" />
-          <input type="file" accept="image/*" onChange={onFileChange} />
+            <input
+                value={nweet}
+                onChange={onChange}
+                type="text"
+                placeholder="What's on your mind?"
+                maxLength={120}
+            />
+            
+            <input type="file" accept="image/*" onChange={onFileChange} />
+            <input type="submit" value="Nweet" />
+            {attachment && (
+            <div>
+                <img src={attachment} width="50px" height="50px" />
+                <button onClick={onClearAttachment}>Clear</button>
+            </div>
+            )}
         </form>
 
         <div>
